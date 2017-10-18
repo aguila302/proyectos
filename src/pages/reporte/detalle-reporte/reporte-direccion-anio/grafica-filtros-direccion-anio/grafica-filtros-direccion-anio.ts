0bd1c261e76c7f26b694bdbbe2e99170e8b675e1
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2017-10-18 17:18:03 -0500

    18 de octubre

--- src/pages/reporte/detalle-reporte/reporte-direccion-anio/grafica-filtros-direccion-anio/grafica-filtros-direccion-anio.ts
+++ src/pages/reporte/detalle-reporte/reporte-direccion-anio/grafica-filtros-direccion-anio/grafica-filtros-direccion-anio.ts
@@ -28,32 +28,43 @@ export class GraficaFiltrosDireccionAnioPage {
 		var series = []
 		series.splice(0, series.length)
 		await this.direcciones.forEach(function callback(item, index) {
+			var res = []
+			miGlobal.dataDirecciones(item, miGlobal.anios).then(x => {
+				x.forEach(item => {
+				console.log('res')
+					res.push(parseFloat(item))
+					
+				})
+			})
+			// console.log(res)
+			
 			series.push({
 				'name': item,
-				'data': miGlobal.dataDirecciones(item, miGlobal.anios)
+				'data': res
 			})
 		})
 		console.log(series);
-		
-		// setTimeout(() => {
-		// 	this.options = this.reporteService.graficaDireccionAniosGeneral(this.anios, series, 'Direcciones')
-		// 	console.log(this.options)
-		// }, 2000)
+
+		setTimeout(() => {
+			this.options = this.reporteService.graficaDireccionAniosGeneral(this.anios, series, 'Direcciones')
+			console.log(this.options)
+		}, 2000)
 	}
 	/* Funcion realizar la consulta necesaria al origen de datos para obtener la data de las direccciones selecionadas.*/
 	async dataDirecciones(direccion, anio) {
 		var miGlobal = this
 		this.data_direcciones.splice(0, this.data_direcciones.length)
+
 		await this.reporteService.obtenerDataFiltracion(direccion, anio)
-		.then(response => {
-			// console.log(response)
-			miGlobal.data_direcciones = response
-			// response.forEach(item => {
-			// 	miGlobal.data_direcciones.push(parseInt(item))
-			// })
-		})
+			.then(response => {
+				// console.log(response)
+				miGlobal.data_direcciones = response
+				// response.forEach(item => {
+				// 	miGlobal.data_direcciones.push(parseInt(item))
+				// })
+			})
 		// console.log('data')
-		console.log(miGlobal.data_direcciones)
+		// console.log(miGlobal.data_direcciones)
 		
 		return miGlobal.data_direcciones
 		
