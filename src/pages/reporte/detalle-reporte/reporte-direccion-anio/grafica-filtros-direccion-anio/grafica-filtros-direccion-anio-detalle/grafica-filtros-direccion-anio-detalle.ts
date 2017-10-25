8d702869706676dbfd6c67e0e6a4358c06f6bbc2
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2017-10-25 13:24:37 -0500

    developing

--- src/pages/reporte/detalle-reporte/reporte-direccion-anio/grafica-filtros-direccion-anio/grafica-filtros-direccion-anio-detalle/grafica-filtros-direccion-anio-detalle.ts
+++ src/pages/reporte/detalle-reporte/reporte-direccion-anio/grafica-filtros-direccion-anio/grafica-filtros-direccion-anio-detalle/grafica-filtros-direccion-anio-detalle.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+	ReportesDbService
+} from '../../../../../../services/reportes.db.service'
 
 @IonicPage()
 @Component({
@@ -10,16 +13,22 @@ export class GraficaFiltrosDireccionAnioDetallePage {
 	direccion: string = ''
 	anio: number = 0
 	monto: string = ''
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	constructor(public navCtrl: NavController, public navParams: NavParams, private reporteService: ReportesDbService) {
 		this.direccion = this.navParams.get('direccion')
 		this.anio = this.navParams.get('anio')
 		this.monto = this.navParams.get('monto')
-		console.log(this.direccion, this.anio, this.monto)
-		
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad GraficaFiltrosDireccionAnioDetallePage');
 	}
 
+	/* Funcion para obtener los proyectos de una direccion y un ano de acuerdo al filtro. */
+	detalleReporte = () => {
+		this.reporteService.reporteDireccionAnioDetalle(this.anio, this.direccion)
+		.then(response => {
+			console.log(response)
+			
+		})
+	}
 }
