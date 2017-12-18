d5c3ec1ed26a76d05088958a1bc45e56a406fa03
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2017-12-18 17:57:16 -0600

    ok

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -173,7 +173,6 @@ export class DbService {
 		let proyectos = []
 		for (let i in filtros) {
 			let sql = 'select * from proyectos where ' + i + ' like ' + "'%" + val + "%' order by nombre_proyecto ASC" 
-			console.log(sql);
 			this.db.executeSql(sql, {})
 				.then((response) => {
 					for (let index = 0; index < response.rows.length; index++) {
