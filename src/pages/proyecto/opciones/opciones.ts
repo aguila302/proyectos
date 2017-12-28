835692e66802e453f396ad44f8f22caf8e9b732e
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2017-12-28 16:56:29 -0600

    ok

--- src/pages/proyecto/opciones/opciones.ts
+++ src/pages/proyecto/opciones/opciones.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Platform } from 'ionic-angular';
 import { AppVersion } from '@ionic-native/app-version';
 import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
 
@@ -13,7 +13,7 @@ export class OpcionesPage {
 	nombreApp: string = ''
 
 	constructor(public navCtrl: NavController, public navParams: NavParams,
-		private appVersion: AppVersion, private document: DocumentViewer, public viewCtrl: ViewController) {
+		private appVersion: AppVersion, private document: DocumentViewer, public viewCtrl: ViewController, public platform: Platform) {
 	}
 
 	// options: DocumentViewerOptions = {
@@ -37,10 +37,21 @@ export class OpcionesPage {
 
 	/* Funcion para ver archivo pdf*/
 	muestraPdf = () => {
+		console.log('pdf')
+		let path: string = ''
+
+		if(this.platform.is('android')) {
+			path = 'file:///android_asset/'
+		}
+
+		if(this.platform.is('ios')) {
+			path = '/var/mobile/Applications/<UUID>/'
+		}
+		
 		const options: DocumentViewerOptions = {
 			title: 'My PDF'
 		}
-		this.document.viewDocument('file:///assets/BIPRO.pdf', 'application/pdf', options)
+		this.document.viewDocument(path + 'BIPRO.pdf', 'application/pdf', options)
 	}
 
 	/* Funcion para cerrar la ventana de opciones */
